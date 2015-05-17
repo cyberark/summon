c915c8b8be2811f095645a9fcb97940d80666d32
Dustin Collins <dustinrcollins@gmail.com>
2015-05-17 12:12:19 -0400

    working build script

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,3 @@ FROM tcnksm/gox:1.4.2-light
 WORKDIR /goroot/src
 RUN GOOS=windows GOARCH=amd64 CGO_ENABLED=0 ./make.bash --no-clean
 RUN GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 ./make.bash --no-clean
-
-ENV GOX_OS "darwin linux windows"
-ENV GOX_ARCH "amd64"
-
-CMD go get -d ./... && gox -verbose -output "pkg/{{.OS}}_{{.Arch}}/{{.Dir}}"
--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@ rm -rf pkg
 
 docker build -t cauldron/build .
 
+projectpath="/goroot/src/github.com/conjurinc/cauldron"
+buildcmd='go get $(<Godeps) && GOX_OS="darwin linux windows" GOX_ARCH="amd64" gox -verbose -output "pkg/{{.OS}}_{{.Arch}}/{{.Dir}}"'
+
 docker run --rm \
--v "$(pwd)":/usr/src/cauldron \
--w /usr/src/cauldron \
-cauldron/build
+-v "$(pwd)":"${projectpath}" \
+-w "${projectpath}" \
+cauldron/build \
+bash -c "${buildcmd}"
