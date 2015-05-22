a454692f4d0064a692fb5be470dc937b9f2f4794
Dustin Collins <dustinrcollins@gmail.com>
2015-05-22 11:17:35 -0400

    fix package building

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ rm -rf pkg
 docker build -t cauldron/build .
 
 projectpath="/goroot/src/github.com/conjurinc/cauldron"
-buildcmd='xargs -L1 go get <Godeps && cd example; GOX_OS="darwin linux windows" GOX_ARCH="amd64" gox -verbose -output "../pkg/{{.OS}}_{{.Arch}}/{{.Dir}}"'
+buildcmd='xargs -L1 go get <Godeps && GOX_OS="darwin linux windows" GOX_ARCH="amd64" gox -verbose -output "pkg/{{.OS}}_{{.Arch}}/{{.Dir}}"'
 
 docker run --rm \
 -v "$(pwd)":"${projectpath}" \
