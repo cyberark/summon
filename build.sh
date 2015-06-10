dbdb53a9abbff212a6a7afdaa91523dbb478cbbc
Dustin Collins <dustinrcollins@gmail.com>
2015-06-10 09:59:06 -0400

    Use Godeps for consistency

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+app="summon"
+
 rm -rf pkg
 
 docker build -t summon/build .
 
-projectpath="/goroot/src/github.com/conjurinc/summon"
-buildcmd='xargs -L1 go get <Godeps && GOX_OS="darwin linux windows" GOX_ARCH="amd64" gox -verbose -output "pkg/{{.OS}}_{{.Arch}}/{{.Dir}}"'
+projectpath="/goroot/src/github.com/conjurinc/${app}"
+buildcmd='GOX_OS="darwin linux windows" GOX_ARCH="amd64" gox -verbose -output "pkg/{{.OS}}_{{.Arch}}/{{.Dir}}"'
 
 docker run --rm \
 -v "$(pwd)":"${projectpath}" \
 -w "${projectpath}" \
+-e "GOPATH=/goroot/src/github.com/conjurinc/${app}/Godeps/_workspace:$GOPATH" \
 summon/build \
 bash -c "${buildcmd}"
