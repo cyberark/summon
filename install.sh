02399d1932a6aa35166a2b7cb79fededae00a950
doodlesbykumbi <kumbirai.tanekha@gmail.com>
2017-08-23 16:23:26 +0100

    fix: fix install.sh in latest version (#50)
    * fix: install.sh by reverting package names to include version and underscores
    
    * update GLOB in package.sh
    
    * update Jenkinsfile
    
    * fix regex in Jenkinsfile
    
    * fix Jenkinsfile
    
    * fix Jenkinsfile
    
    * fix permission on prepare-acceptance
    
    * globbing didn't work, use loop ._.
    
    * try again ._.
    
    * update install.sh to accomodate new build pattern
    
    * revert GLOB to new package build pattern

--- install.sh
+++ install.sh
@@ -46,10 +46,9 @@ get_latest_version() {
   echo "${version}"
 }
 
-LATEST_VERSION=$(get_latest_version 'https://raw.githubusercontent.com/cyberark/summon/master/version.go')
-BASEURL="https://github.com/cyberark/summon/releases/download/"
-URL=${BASEURL}"v${LATEST_VERSION}/summon_v${LATEST_VERSION}_${DISTRO}_amd64.tar.gz"
-
+LATEST_VERSION=$(get_latest_version 'https://raw.githubusercontent.com/conjurinc/summon/master/version.go')
+BASEURL="https://github.com/conjurinc/summon/releases/download/"
+URL=${BASEURL}"v${LATEST_VERSION}/summon-${DISTRO}-amd64.tar.gz"
 
 ZIP_PATH="${tmp_dir}/summon.tar.gz"
 do_download ${URL} ${ZIP_PATH}
