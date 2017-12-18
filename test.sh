a5d7214b173e3e326292e3d44167a7ede25f10af
Kumbirai Tanekha <kumbirai.logistics@gmail.com>
2017-12-18 00:25:42 +0000

    test.sh should exit accordingly when test fails

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@ echo "Running unit tests"
 docker-compose build --pull summon-builder
 
 docker-compose run --rm --entrypoint bash summon-builder \
-  -c 'go test -v ./... | tee junit.output && cat junit.output | go-junit-report > output/junit.xml'
+  -c 'go test -v ./... | tee junit.output && cat junit.output | go-junit-report --set-exit-code=true > output/junit.xml'
