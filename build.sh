ee6d97091ea98b5c4553e57fa8cde48c1bcf25f0
Dustin Collins <dustin.collins@cyberark.com>
2018-08-06 13:47:25 -0500

    Use goreleaser to build and package summon

--- build.sh
+++ build.sh
@@ -1,36 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
 
-# Platforms to build: https://golang.org/doc/install/source#environment
+git fetch --tags  # jenkins does not do this automatically yet
 
-DEFAULT_PLATFORMS=(
-  'darwin:amd64'     # MacOS
-  'freebsd:amd64'
-  'linux:amd64'
-  # 'linux:arm'
-  # 'linux:arm64'
-  'netbsd:amd64'
-  'openbsd:amd64'
-  'solaris:amd64'
-  # 'windows:386'
-  'windows:amd64'
-)
-PLATFORMS="${1:-${DEFAULT_PLATFORMS[@]}}"  # override this with a positional argument, like 'linux:amd64'
+docker-compose pull goreleaser
 
-OUTPUT_DIR='output'
-
-echo "Creating summon binaries in $OUTPUT_DIR/"
-docker-compose build --pull summon-builder
-
-for platform in ${PLATFORMS}; do
-  GOOS=${platform%%:*}
-  GOARCH=${platform#*:}
-
-  echo "-----"
-  echo "GOOS=$GOOS, GOARCH=$GOARCH"
-  echo "....."
-
-  docker-compose run --rm \
-    -e GOOS=$GOOS -e GOARCH=$GOARCH \
-    summon-builder \
-    build -v -o $OUTPUT_DIR/summon-$GOOS-$GOARCH
-done
+docker-compose run --rm goreleaser release --rm-dist --skip-validate
